d9fb73fd79e6c1eaf18bc99cd8f2897cdf2dec70
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-07-25 09:19:50 -0700

    init

---
+++ src/main/java/com/sz/cardly/CardlyApplication.java
@@ -0,0 +1,13 @@
+package com.sz.cardly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sz/cardly/CardlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sz.cardly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
