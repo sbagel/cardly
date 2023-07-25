62139d3b516ba8db9d607ad125874fb6f4d5de84
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-07-25 10:07:11 -0700

    adding User dto

---
+++ src/main/java/com/sz/cardly/entities/User.java
@@ -0,0 +1,11 @@
+package com.sz.cardly.entities;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String username;
+    private String name;
+    private String photo;
+}
