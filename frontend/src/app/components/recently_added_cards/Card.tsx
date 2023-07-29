f7f7d43dbc9ce1e601220f88aabb5cc5ea2f2d98
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-07-28 19:00:33 -0700

    editing cards

--- frontend/src/app/components/recently_added_cards/Card.tsx
+++ frontend/src/app/components/recently_added_cards/Card.tsx
@@ -1,5 +1,7 @@
-import { createStyles, NavLink, rem, Divider } from '@mantine/core';
-import { useInputState, useHover } from '@mantine/hooks';
+import { useRef, useState } from 'react';
+import { useForm } from '@mantine/form';
+import { createStyles, Textarea, NavLink, rem, Divider, Button } from '@mantine/core';
+import { useHover } from '@mantine/hooks';
 import { Card as CardType } from "../../../types/CardTypes";
 import useCardsFacade from '../../facades/useCardsFacade';
 import { FaEllipsis, FaPenToSquare, FaTrashCan } from "react-icons/fa6";
@@ -10,7 +12,6 @@ const useStyles = createStyles((theme) => ({
     fontSize: rem(18),
     fontWeight: 500,
     padding: rem(20),
-    border: `${rem(3)} solid black`,
     borderRadius: '20px',
     display: 'flex',
     alignItems: 'center',
@@ -20,9 +21,12 @@ const useStyles = createStyles((theme) => ({
     position: 'relative',
   },
   value: {
-    width: '100%',
+    width: '98%',
     padding: rem(20),
-    fontWeight: 600,
+    fontWeight: 500,
+    borderRadius: '20px',
+    margin: 'auto',
+    fontSize: rem(20)
   },
   ellipsisContainer:{
     padding: rem(20),
@@ -42,6 +46,11 @@ const useStyles = createStyles((theme) => ({
     color: theme.colorScheme === 'dark' ? theme.white : theme.black,
     backgroundColor: theme.colorScheme === 'dark' ? theme.black : theme.white,
     fontSize: rem(20)
+  },
+  btn: {
+    position: 'absolute',
+    visibility: 'hidden',
+    height:0,
   }
 }));
 
@@ -50,27 +59,83 @@ interface CardProps {
 }
 
 export default function Card({ card }: CardProps) {
-  const { deleteCard } = useCardsFacade();
+  const { deleteCard, updateCard } = useCardsFacade();
 
   const { classes } = useStyles();
 
+  const [edit, setEdit] = useState(false);
   const { hovered, ref } = useHover();
 
-  const [termValue, setTermValue] = useInputState(card.front);
-  const [definitionValue, setDefinitionValue] = useInputState(card.back);
+  const form = useForm({
+    initialValues: { id: card.id, deckId: card.deckId, front: card.front, back: card.back, favorited: card.favorited},
+
+    validate: {
+      front: (value) => (value.trim().length < 1 || value.split(" ").length < 2 ? 'Card term must have at least 2 words' : null),
+      back: (value) => (value.trim().length < 1 ? 'Card defintion is empty' : null),
+    },
+  });
+
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  const handleEnter = (event: React.KeyboardEvent<HTMLFormElement>) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      btnRef.current?.click();
+      exitEdit();
+    }
+  };
+
+  const exitEdit = () => {
+    if (form.isValid('front') && form.isValid('back')) {
+      setEdit(false);
+    }
+  }
 
   return (
-    <div className={classes.card} ref={ref}>
+    <div className={classes.card} ref={ref} style={{border: edit ? `${rem(6)} solid #d6e7fe` : `${rem(3)} solid black`,}}>
+
     {hovered &&
      <div className={classes.ellipsisContainer}>
       <NavLink component="div" className={classes.ellipsisIcon} childrenOffset={28} rightSection={<FaEllipsis/>}>
-        <NavLink component="div" className={classes.editCardLabel} icon={<FaPenToSquare size={15}/>} label="Edit card" />
+        <NavLink component="div" onClick={() => setEdit(true)} className={classes.editCardLabel} icon={<FaPenToSquare size={15}/>} label="Edit card" />
         <NavLink component="div" onClick={() => deleteCard(card.id)} className={classes.editCardLabel} icon={<FaTrashCan size={15}/>} label="Delete card" />
       </NavLink>
     </div>}
-    <div className={classes.value}>{termValue}</div>
-      <Divider style={{width:'96%'}} color="gray.2" my="xs" />
-    <div className={classes.value}>{definitionValue}</div>
-  </div>
+
+      <div style={{width: '100%', pointerEvents: edit ? 'auto' : 'none'}}>
+        {!edit && <>
+          <div className={classes.value}>{card.front}</div>
+          <Divider style={{width:'96%', margin: 'auto'}} color="gray.2" my="xs" />
+        <div className={classes.value}>{card.back}</div></>}
+
+        {edit &&
+          <form onSubmit={form.onSubmit(updateCard)} onKeyUp={handleEnter} onKeyDown={event => {if (event.key === "Enter" && !event.shiftKey) event.preventDefault()}}>
+          <Textarea
+            autosize
+            className={classes.value}
+            variant='unstyled'
+            sx={(theme) => ({backgroundColor: edit ? theme.colors.gray[0] : 'inherit', marginBottom: edit ? '20px' : 'auto'})}
+            styles={{input: {fontSize: rem(20)}}}
+            minRows={1}
+            {...form.getInputProps('front')}
+          />
+
+            <Divider style={{width:'96%', margin: 'auto'}} color="gray.2" my="xs" />
+
+          <Textarea
+            autosize
+            className={classes.value}
+            variant='unstyled'
+            sx={(theme) => ({backgroundColor: edit ? theme.colors.gray[0] : 'inherit', marginTop: edit ? '20px' : 'auto'})}
+            styles={{input: {fontSize: rem(20)}}}
+            minRows={1}
+            {...form.getInputProps('back')}
+          />
+
+          <Button ref={btnRef} type="submit" mt="sm" className={classes.btn}></Button>
+        </form>
+        }
+      </div>
+    </div>
   )
 }
